2072d817bcb2d5d3cb873c81d41e52a78d759da5
codingNonGuru <andreialbu50@yahoo.com>
2018-10-13 15:37:06 +0300

    Add Citizen Spawner

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CitizenGroupData
+{
+	public int count;
+	public float direction;
+}
+
+[Serializable]
+public class CitizenWaveData
+{
+	public List <CitizenGroupData> groups;
+}
+
+public class SpawnManager : MonoBehaviour 
+{
+	[SerializeField]
+	List <CitizenWaveData> waveDatas = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
